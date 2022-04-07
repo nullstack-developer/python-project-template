b06b72a1571cfc2a1751e93779108669aa88ab93
kbpark-onepredict <kbpark@onepredict.com>
2022-04-07 16:12:40 +0900

    feat: change logging module

--- python_project/utils/logger.py
+++ python_project/utils/logger.py
@@ -1,14 +1,12 @@
 import logging
 import sys
-from logging.handlers import RotatingFileHandler
 from pathlib import Path
 from types import TracebackType
 from typing import Optional
 
-DEBUG_LOG_PATH = Path("./log/debug.log")
-DEBUG_LOG_MAX_SIZE = 10 * 1024 * 1024
-DEBUG_LOG_COUNT = 10
-ERROR_LOG_PATH = Path("./log/error.log")
+LOG_DIR_PATH = Path("./log")
+LOG_DIR_PATH.mkdir(exist_ok=True, parents=True)
+LOG_PATH = LOG_DIR_PATH / "log.log"
 
 
 def make_logger(name: Optional[str]) -> logging.Logger:
@@ -24,7 +22,7 @@ def make_logger(name: Optional[str]) -> logging.Logger:
         logging.Logger: Logging instance.
     """
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
     formatter = logging.Formatter(
         "[%(asctime)s] %(levelname)-8s >> %(name)s : %(lineno)4s - %(message)s"
     )
@@ -34,39 +32,22 @@ def make_logger(name: Optional[str]) -> logging.Logger:
     console.setFormatter(formatter)
     logger.addHandler(console)
 
-    debug_file_handler = RotatingFileHandler(
-        DEBUG_LOG_PATH,
-        maxBytes=DEBUG_LOG_MAX_SIZE,
-        backupCount=DEBUG_LOG_COUNT,
-    )
-    debug_file_handler.setLevel(logging.DEBUG)
-    debug_file_handler.setFormatter(formatter)
-    logger.addHandler(debug_file_handler)
+    file_handler = logging.FileHandler(filename=LOG_PATH)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
-    error_file_handler = logging.FileHandler(filename=ERROR_LOG_PATH)
-    error_file_handler.setLevel(logging.DEBUG)
-    error_file_handler.setFormatter(formatter)
-    logger.addHandler(error_file_handler)
-    return logger
+    def hook_exception(
+        exc_type,
+        exc_value: BaseException,
+        exc_traceback: Optional[TracebackType],
+    ) -> None:
+        if issubclass(exc_type, KeyboardInterrupt):
+            sys.__excepthook__(exc_type, exc_value, exc_traceback)
 
+        logger.exception(
+            "Unexpected exception", exc_info=(exc_type, exc_value, exc_traceback)
+        )
 
-def hook_exception(
-    exc_type,
-    exc_value: BaseException,
-    exc_traceback: Optional[TracebackType],
-) -> None:
-    if issubclass(exc_type, KeyboardInterrupt):
-        sys.__excepthook__(exc_type, exc_value, exc_traceback)
-
-    logger.error("Unexpected exception", exc_info=(exc_type, exc_value, exc_traceback))
-
-
-if __name__ == "__main__":
-    logger = make_logger(__name__)
     sys.excepthook = hook_exception
-    logger.debug("debug")
-    logger.info("info")
-    logger.warning("warning")
-    logger.error("error")
-    logger.critical("critical")
-    raise RuntimeError("Test unhandled")
+    return logger
